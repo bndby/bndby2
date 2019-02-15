f0a54bcc380e273425ba601a3f8f85acab1ab2e0
Yuri Bondarenko <by@klen.by>
2019-02-15 09:29:22 +0300

    postcss cssnext

--- components/Age/index.js
+++ components/Age/index.js
@@ -1,9 +1,11 @@
+import './age.css'
+
 const wasBurn = new Date( 1981, 10, 16, 19, 55, 0, 0 )
 const now = new Date
 const age = new Date( Math.abs( now - wasBurn ) )
 
 const Age = () => (
-	<span>{ age.getDate() - 1 }.{ age.getMonth() < 10 ? '0' : '' }{ age.getMonth() }.{ age.toISOString().slice(0, 4) - 1970 } { age.getHours() }:{ age.getMinutes() }</span>
+	<span className="age">{ age.getDate() - 1 }.{ age.getMonth() < 10 ? '0' : '' }{ age.getMonth() }.{ age.toISOString().slice(0, 4) - 1970 } { age.getHours() }:{ age.getMinutes() }</span>
 )
 
 export default Age
