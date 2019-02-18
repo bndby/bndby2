6d74ee77d75bceb843107afe206e434081880717
Yuri Bondarenko <by@klen.by>
2019-02-18 13:37:53 +0300

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,10 @@
 // next.config.js
 const withCSS = require('@zeit/next-css')
 module.exports = withCSS({
-	
-	cssModules: true,
-	webpack: function (config) {
-		return config;
-	},
-
 	exportPathMap: function () {
 		return {
 			'/': { page: '/' },
-			'/deploy/': {page: '/deploy'}
+			'/deploy': {page: '/deploy'}
 		}
 	}
 })
