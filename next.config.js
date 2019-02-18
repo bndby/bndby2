058dd0265249173b06bc9cba989507e16c4137d6
Yuri Bondarenko <by@klen.by>
2019-02-18 10:47:01 +0300

    map, build config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 // next.config.js
 const withCSS = require('@zeit/next-css')
-module.exports = withCSS()
+module.exports = withCSS({
+	
+	cssModules: true,
+	webpack: function (config) {
+		return config;
+	},
+
+	exportPathMap: function () {
+		return {
+			'/': { page: '/' },
+			'/deploy/': {page: '/deploy'}
+		}
+	}
+})
