4ea5307ad29d50e75f90cc46ec2e89d3243f1a18
By <by@klen.by>
2019-02-24 01:53:48 +0300

    server

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const port = process.env.PORT || 3000
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-const cors = require( 'cors' )
+//const cors = require( 'cors' )
 
 app.prepare()
 	.then( () => {
 		const server = express()
 
-		server.use( cors() )
+		//server.use( cors() )
 
 		server.get( '*', ( req, res ) => {
 			return handle(req, res)
