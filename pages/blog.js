c695d16c16dcb00397260a52b2c8e0007879776e
Yuri Bondarenko <by@klen.by>
2019-03-11 21:11:02 +0300

    ytrt

--- pages/blog.js
+++ pages/blog.js
@@ -1,22 +1,45 @@
+/**
+ * 
+ */
 import Head from 'next/head'
 import Layout from '../components/Layout'
 import { i18n, Link, withNamespaces } from '../i18n'
 import React from 'react'
 import PropTypes from 'prop-types'
 
+/**
+ * 
+ */
 const PostLink = ( props ) => (
-	<li>
+	<p>
+		{ 
+			props.date ?
+				<><time>{props.date}</time><br /></> :
+				''
+		}
 		<Link as={`/post/${props.slug}`} href={`/post?slug=${props.slug}`}>
 			<a>{props.title}</a>
 		</Link>
-	</li>
+		{
+			props.children ?
+				<><br />{ props.children }</> :
+				''
+		}
+	</p>
 )
 
+/**
+ * 
+ */
 PostLink.propTypes = {
 	slug: PropTypes.string.isRequired,
-	title: PropTypes.string.isRequired
+	title: PropTypes.string.isRequired,
+	date: PropTypes.string
 }
 
+/**
+ * 
+ */
 class Blog extends React.Component {
 
 	static async getInitialProps() {
@@ -34,13 +57,23 @@ class Blog extends React.Component {
 				<Head>
 					<title>{ t( 'blog' ) }</title>
 				</Head>
+
 				<h1>{ t( 'blog' ) }</h1>
-				<ul>
-					<PostLink slug="blog-slug" title="Blog post" />
-				</ul>
+
+				<PostLink slug="blog-slug" title="Blog post" date="11.03.2019">
+					Описание записи блога
+				</PostLink>
+
 			</Layout>
 		)
 	}
 }
 
+Blog.propTypes = {
+	t: PropTypes.func
+}
+
+/**
+ * 
+ */
 export default withNamespaces( 'common' )( Blog )
