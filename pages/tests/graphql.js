53a09d1a6687159ebd51cc92007417cd6929ab20
Bandarenka Yura <by@klen.by>
2019-05-08 11:30:46 +0300

    up

--- pages/tests/graphql.js
+++ pages/tests/graphql.js
@@ -2,50 +2,62 @@ import React from 'react'
 import fetch from 'isomorphic-unfetch'
 import Layout from '../../components/Layout'
 import ReactMarkdown from 'react-markdown'
+import { i18n, withNamespaces } from '../../i18n'
+import PropTypes from 'prop-types'
 
 /**
  * 
  * @param {*} props 
  */
-const WpAPI = (props) => (
+const GraphQL = (props) => (
 	<Layout>
+
 		<h1>GraphQL</h1>
 
 		<ReactMarkdown source={props.pages[0].node.content} />
 		
-
 	</Layout>
 )
   
-WpAPI.getInitialProps = async function() {
-	const query = `{
-		allPage (locale: "ru") {
-			edges {
-				node {
-					title,
-					slug,
-					content,
-					_meta {
-						id
-					}
-				}
+GraphQL.getInitialProps = async function( context ) {
+
+	const locale = context.req ? context.req.locale : 'en'
+
+	const query = `query cvPage ($slug: String) {
+		allPage (locale: "${locale}", where:{
+		  slug: {
+			eq: $slug
+		  }
+		}) {
+		  edges {
+			node {
+			  title,
+			  content
 			}
+		  }
 		}
-	}`
+	  }`
 	const res = await fetch('https://bndby-prime.herokuapp.com/graphql', {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify({
-			query: query
+			query: query,
+			'slug': 'cv'
 		}),
 	})
 	const data = await res.json()
   
-	console.log(`Show data fetched. Count: ${data.length}`)
-  
 	return {
-		pages: data.data.allPage.edges
+		pages: data.data.allPage.edges,
+		namespacesRequired: ['common']
 	}
 }
 
-export default WpAPI
+/**
+ * 
+ */
+GraphQL.propTypes = {
+	pages: PropTypes.array
+}
+
+export default withNamespaces( 'common' )( GraphQL )
