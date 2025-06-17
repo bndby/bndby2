1e11f8dac87564067e654c24ba25943eb0d3d65f
Bandarenka Yura <ydaren@gmail.com>
2025-06-18 00:14:50 +0300

    fix deploy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
   },
   // Современные настройки для Next.js 15
   poweredByHeader: false,
   compress: true,
+  trailingSlash: true,
 };
 
 export default nextConfig;
